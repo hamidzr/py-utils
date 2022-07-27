77c264760b8776caec1ec2ca2c77d9aa235f6418
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-07-26 18:06:01 -0700

    add some bash utilities

---
+++ src/py_utils/bash.py
@@ -0,0 +1,37 @@
+import subprocess
+import pathlib
+import typing as t
+
+# print colored text
+def print_colored(skk): print("\033[93m {}\033[00m" .format(skk))
+
+
+def run(command, cwd: t.Optional[pathlib.Path] = None):
+    msg = command
+    if cwd is not None:
+        msg = f'{command} [cwd: {cwd}]'
+    print_colored(msg)
+    subprocess.run(command, cwd=cwd, check=True, shell=True)
+
+
+def has_output(command):
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    return len(result.stdout) > 0
+
+
+def fails(command):
+    try:
+        subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        return True
+    return False
+
+# read command output
+def get_output(command):
+    result = subprocess.run(command, shell=True, stdout=subprocess.PIPE)
+    return result.stdout.decode('utf-8')
+
+
+# get current git hash
+def get_current_hash():
+    return get_output('git rev-parse HEAD')
