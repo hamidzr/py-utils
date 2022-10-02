0b601990df4868891fc2fe89ef631a40a2e53ffb
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2022-10-02 14:35:46 -0700

    debounce decorator

--- src/py_utils/v1/proc.py
+++ src/py_utils/v1/proc.py
@@ -1,5 +1,6 @@
 import subprocess
 import typing as t
+import threading
 
 
 def exits_with(code: int, cmd: t.List[str]) -> bool:
@@ -8,3 +9,22 @@ def exits_with(code: int, cmd: t.List[str]) -> bool:
         return cp.returncode == code
     except subprocess.CalledProcessError as e:
         return e.returncode == code
+
+
+# debounce decorator with args and kwargs in miliseconds
+def debounce(wait: int):
+    def decorator(fn):
+        def debounced(*args, **kwargs):
+            def call_it():
+                fn(*args, **kwargs)
+
+            try:
+                debounced.t.cancel()
+            except AttributeError:
+                pass
+            debounced.t = threading.Timer(wait / 1000, call_it)
+            debounced.t.start()
+
+        return debounced
+
+    return decorator
