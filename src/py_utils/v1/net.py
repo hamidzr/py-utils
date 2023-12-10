3e7e4c26da0dfab9ce82a6988d45edc94e41173f
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2023-12-10 11:06:34 -0800

    add a util to get the first network hop

---
+++ src/py_utils/v1/net.py
@@ -0,0 +1,13 @@
+import subprocess
+import re
+from typing import Optional
+
+def get_first_hop_ip() -> Optional[str]:
+    # run traceroute to a common address like google
+    result = subprocess.run(["traceroute", "-m", "1", "-w", "1","8.8.8.8"], stdout=subprocess.PIPE, text=True, stderr=subprocess.DEVNULL)
+
+    # use regular expression to find IP addresses
+    ips = re.findall(r'\d+\.\d+\.\d+\.\d+', result.stdout)
+
+    # return the first IP address found, which is the first hop
+    return ips[0] if ips else None
