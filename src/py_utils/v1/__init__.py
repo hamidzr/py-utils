f70ec31b5505d8de0814d3c06a9e8721eee16a00
Hamid Zare <12127420+hamidzr@users.noreply.github.com>
2024-04-06 12:35:10 -0500

    add ping and net check to net

---
+++ src/py_utils/v1/__init__.py
@@ -0,0 +1,2 @@
+from py_utils.v1 import (bash, cli, config, file, log, net, proc, render,
+                         xdotool)
--- src/py_utils/v1/net.py
+++ src/py_utils/v1/net.py
@@ -18,3 +18,25 @@ def get_first_hop_ip() -> Optional[str]:
 
     # return the first IP address found, which is the first hop
     return ips[0] if ips else None
+
+
+def ping_server(ip_address: str, timeout: int = 1) -> Optional[float]:
+    """
+    Ping a server and return the time in milliseconds.
+    timeout: time to wait for a response in seconds
+    """
+    response = subprocess.Popen(
+        ["ping", "-c", "1", "-t", str(timeout), ip_address], stdout=subprocess.PIPE
+    )
+    output = response.communicate()[0].decode("utf-8")
+
+    try:
+        ping_time = float(output.split("time=")[1].split(" ms")[0])
+    except:
+        ping_time = None
+
+    return ping_time
+
+
+def can_reach_internet() -> bool:
+    return ping_server("8.8.8.8", timeout=1) is not None
